70758f6935eb784887bbb88fc6a879e1146d1022
jmg <macgreene14@gmail.com>
2022-07-23 12:23:33 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+
+// This script creates a randomly generated password based on user selected criteria
+// Mac Greene July 23, 2022
+
+var btn = document.getElementById("button");
+btn.addEventListener("click", createPassword);
+
+function createPassword() {
+var alphabetLower = "abcdefghijklmnopqrstuvwxyz"
+var alphabetUpper = alphabetLower.toUpperCase()  //create uppcase string
+var numbers = "123456789"
+var chars = "" // create empty string for appending selected characters
+var input = prompt("Enter Desired Length of Password, between 8 to 128: ") //todo data validation
+var charInput = false;
+
+while (parseInt(input) < 8 || parseInt(input) > 128 || isNaN(parseInt(input))) {
+    input = prompt("Error, Please Enter Length of Password between 8 to 128: ")
+    if (input === null) break ;  //break if cancel is selected
+}
+
+if (input !== null) {
+    var upper = confirm("Include Uppercase? Confirm for Yes, Cancel for No.")
+    var lower = confirm("Include Lowercase? Confirm for Yes, Cancel for No.")
+    var numerics= confirm("Include Numerics? Confirm for Yes, Cancel for No.")
+    // var special= confirm("Include Special Characters?")
+
+    if (upper) {
+        chars = chars + alphabetUpper;
+        charInput = true;
+    } if (lower) {
+        chars = chars + alphabetLower ;
+        charInput = true;
+    } if (numerics) {
+        chars = chars + numbers;
+        charInput = true;
+    } 
+
+    if (charInput) {
+        var password = "" 
+        var index = 0
+
+        for (var i = 0; i < Number(input); i++)
+        {
+            index = Math.floor(Math.random() * chars.length)
+            password = password +  chars[index]
+            console.log(chars[index])
+        }
+        console.log(password)
+
+        var div = document.getElementById("passwordElement");
+        var textElement = document.createElement("h2");
+        textElement.textContent = password;
+        div.appendChild(textElement);   
+        alert("Password Created Successfully!")
+
+    } else {
+        alert("Canceled. Refresh to Create A Password!");
+    }
+    
+} else {
+    alert("Canceled. Refresh to Create A Password!");
+} 
+}
