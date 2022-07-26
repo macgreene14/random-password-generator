691390f4e5f0c10f263d7d29177d005f1dbb451e
jmg <macgreene14@gmail.com>
2022-07-25 20:12:53 -0500

    Added media query and updated styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,8 @@ var chars = "" // create empty string for appending selected characters
 var input = prompt("Enter Desired Length of Password, between 8 to 128: ") //todo data validation
 var charInput = false;
 
+// todo improve data validation so that cancel does not need to be pushed twice on initial dialog box
+
 while (parseInt(input) < 8 || parseInt(input) > 128 || isNaN(parseInt(input))) {
     input = prompt("Error, Please Enter Length of Password between 8 to 128: ")
     if (input === null) break ;  //break if cancel is selected
@@ -51,13 +53,13 @@ if (input !== null) {
         var textElement = document.createElement("h2");
         textElement.textContent = password;
         div.appendChild(textElement);   
-        alert("Password Created Successfully!")
+        // alert("Password Created Successfully!")
 
     } else {
-        alert("Canceled. Refresh to Create A Password!");
+        alert("No Criteria Selected");
     }
     
 } else {
-    alert("Canceled. Refresh to Create A Password!");
+    alert("Canceled");
 } 
 }
